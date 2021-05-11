fb7cd07b1026c66641d83028f9ffc3410daa6b79
Rodrigo-sky <rtrs.eu@gmail.com>
2021-05-10 23:08:00 -0300

    ...

--- Semana 1 - coursera USP 2/soma_matriz.py
+++ Semana 1 - coursera USP 2/soma_matriz.py
@@ -1,19 +1,21 @@
 def soma_matrizes(m1, m2):
 
    if (len(m1) != len(m2)):
-      print(False)
+      return False
    else:
       matriz = []
       for i in range(len(m1)):
+         if (len(m1[i]) != len(m2[i])):
+            return False
          linha = []
          for x in range(len(m1[i])):
             linha.append(m1[i][x] + m2[i][x])
          matriz.append(linha)
-      print(matriz)
+      return matriz
 
 
-m1 = [[1, 2, 3], [4, 5, 6]]
-m2 = [[2, 3, 4], [5, 6, 7]]
+# m1 = [[1, 2, 3], [4, 5, 6]]
+# m2 = [[2, 3, 4], [5, 6, 7]]
 
-soma_matrizes(m1, m2)
+# soma_matrizes(m1, m2)
  
