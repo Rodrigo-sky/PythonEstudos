afce817c23e4288627bec118fdb60d1f9c9ff6af
Rodrigo-sky <rtrs.eu@gmail.com>
2021-05-13 10:20:42 -0300

    em resolucao

--- Semana 1 - coursera USP 2/imprime_matriz.py
+++ Semana 1 - coursera USP 2/imprime_matriz.py
@@ -1,9 +1,17 @@
+# def imprime_matriz(matriz):
+#    for i in matriz:
+#       for x in i:
+#          print(x, end=" ")
+#       print("")
+
 def imprime_matriz(matriz):
    for i in matriz:
-      for x in i:
-         print(x, end=" ")
-      print("")
-      
+      for x in range(len(i)):
+         print(i[x], end="")
+         if x < len(i):
+            print(" ",end="")
+            
+         
 
-minha_matriz = ([[1, 2, 7], [3, 4, 8], [1, 2, 3]])
-imprime_matriz(minha_matriz)
+# minha_matriz = ([[1, 2, 7], [3, 4, 8], [1, 2, 3]])
+# imprime_matriz(minha_matriz)
